bf1332bb975411fd099bd69e8e7835f74e81d9a4
Jerome Chan <eviltofu@mac.com>
2024-08-29 01:43:50 +0800

    Added basic tests skeleton

---
+++ Tests/property-wrapperTests/property_wrapperTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+import Combine
+@testable import property_wrapper
+
+final class property_wrapperTests: XCTestCase {
+    func testClass() {
+        XCTFail()
+    }
+    func testStruct() {
+        XCTFail()
+    }
+    func testEnum() {
+        XCTFail()
+    }
+    func testActor() {
+        XCTFail()
+    }
+}
